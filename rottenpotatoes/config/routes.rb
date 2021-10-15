09c48519ceeaef38ef69782bf01c79075090acc4
Zhale Nowroozilarki <76499517+zhalenl@users.noreply.github.com>
2021-10-14 20:58:02 -0500

    Part2 Done

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,5 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
+  get '/movies/:id/search', :to => 'movies#search', :as => :search
   root :to => redirect('/movies')
+  
 end
